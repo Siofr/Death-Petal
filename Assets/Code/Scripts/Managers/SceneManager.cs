c86d1db92ec7b68538df692d38165cae7b7f2561
nkoles <37773744+nkoles@users.noreply.github.com>
2025-11-19 14:07:59 +0000

    Scene additional stuff added

--- Assets/Code/Scripts/Managers/SceneManager.cs
+++ Assets/Code/Scripts/Managers/SceneManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public struct LevelLoadingEvent : IEvent
@@ -22,30 +23,61 @@ public struct LevelDeloadedEvent : IEvent
 
 public class SceneManager : Singleton<SceneManager>
 {
-    [Header("Levels")] 
-    [SerializeField] private Transform _levelContainer;
+    [Header("Levels")] [SerializeField] private Transform _levelContainer;
     [SerializeField] private GameObject[] _levelPrefabs;
-    [SerializeField] private int _currentLevelID;
-    
-    [Header("Managers")] 
-    [SerializeField] private Transform _managerContainer;
+
+    [Header("Managers")] [SerializeField] private Transform _managerContainer;
     [SerializeField] private GameObject[] _managerPrefabs;
-    
-    [Header("UI")] 
-    [SerializeField] private Transform _uiContainer;
+
+    [Header("UI")] [SerializeField] private Transform _uiContainer;
     [SerializeField] private GameObject[] _uiPrefabs;
-    
-    [Header("Player")] 
-    [SerializeField] private Transform _playerContainer;
+
+    [Header("Player")] [SerializeField] private Transform _playerContainer;
     [SerializeField] private GameObject _playerPrefab;
-    
+
+    //Non-Serializable Fields
+    private int _currentLevelID;
+
     //Properties
     public GameObject[] Levels => _levelPrefabs;
     public GameObject[] Managers => _managerPrefabs;
     public GameObject[] UI => _uiPrefabs;
+
+    public async Task LoadLevel(int id)
+    {
+        
+    }
+
+    public async Task LoadUI(int id)
+    {
+        
+    }
+
+    public async Task LoadUI(string name)
+    {
+        for (int i = 0; i < _uiPrefabs.Length; i++)
+        {
+            if(_uiPrefabs[i].name == name)
+            {
+                await LoadUI(i);
+                return;
+            }
+        }
+    }
+    
+    public async Task LoadManager(int id)
+    {
+    }
+
+    public async Task UnloadLevel(int id)
+    {
+        
+    }
+
+    public async Task ReloadLevel()
+    {
+        await LoadLevel(_currentLevelID);
+    }
 }
 
-public class LevelManager : Singleton<LevelManager>
-{
-    
-}
+public abstract class EntitySaveData_SO: ScriptableObject { }
