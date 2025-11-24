722cbca74dff99329f75e8d22652995721c35622
nkoles <37773744+nkoles@users.noreply.github.com>
2025-11-24 02:05:32 +0000

    hitfeedback added

---
+++ Assets/Code/Scripts/Player/Monobehaviour/HitFeedback.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class HitFeedback : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float intensity;
+    [Range(0f, 5f)]
+    public float duration;
+
+    private Coroutine _hitEffect = null;
+
+    public VolumeProfile globalProfile;
+
+    private Vignette _vignette;
+
+    private void Awake()
+    {
+        globalProfile.TryGet<Vignette>(out _vignette);
+    }
+    
+    public void OnEnemyHit()
+    {
+        print(_hitEffect);
+
+        if (_hitEffect == null)
+            _hitEffect = StartCoroutine(HitEffect());
+    }
+
+    private IEnumerator HitEffect()
+    {
+        print("entered");
+
+        _vignette.active = true;
+        _vignette.intensity.value = 0;
+
+        float lerp = 0f;
+        
+        while(lerp < 1f)
+        {
+            lerp += Time.deltaTime / duration * 2;
+            _vignette.intensity.value = Mathf.Lerp(0f, intensity, lerp);
+            yield return null;
+        }
+
+        lerp = 0f;
+        _vignette.intensity.value = intensity;
+
+        while (lerp < 1f)
+        {
+            lerp += Time.deltaTime / duration * 2;
+            _vignette.intensity.value = Mathf.Lerp(intensity, 0f, lerp);
+            yield return null;
+        }
+
+        _vignette.intensity.value = 0f;
+        print("yurr");
+
+        _vignette.active = false;
+        _vignette.intensity.value = 0f;
+
+        _hitEffect = null;
+    }
+}
