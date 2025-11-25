e10f69a67a78569039254970df858a7a27c08942
MalinaZhnita <simon.kondela@gmail.com>
2025-11-25 12:10:30 +0000

    WieghtPuzzleWrongHit

---
+++ Assets/Code/Scripts/VFX/PuzzleEffects.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleEffects : MonoBehaviour
+{
+    private EventBindings<WrongShotPuzzleEvent> _wrongShotPuzzleEventListener;
+    
+    public List<GameObject> wrongShotEffectObjects;
+
+    private void OnEnable()
+    {
+        _wrongShotPuzzleEventListener = new EventBindings<WrongShotPuzzleEvent>(OnWrongShot);
+        EventBus<WrongShotPuzzleEvent>.Register(_wrongShotPuzzleEventListener);
+    }
+
+    private void OnDisable()
+    {
+        EventBus<WrongShotPuzzleEvent>.Unregister(_wrongShotPuzzleEventListener);
+    }
+
+    private void OnWrongShot(WrongShotPuzzleEvent ctx)
+    {
+        print("wrong shot particles spawning");
+        if (ctx.weight != GetComponentInParent<Weight>())
+            return;
+
+        foreach (var effect in wrongShotEffectObjects)
+        {
+            effect.SetActive(true);
+        }
+    }
+}
