e743a1a3d27e2b8f200288f26a6bfc57bcb04539
Jessie Kelly <everydyingplanet@gmail.com>
2026-03-11 16:12:40 +0000

    New character emotions

--- Assets/YarnSpinner/Actors/ActorSprites.cs
+++ Assets/YarnSpinner/Actors/ActorSprites.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ActorSprites", menuName = "Scriptable Objects/ActorSprites")]
 public class ActorSprites : ScriptableObject
 {
-    public Sprite actorSprite;
+    public int actorID;
+    public Sprite characterDefault;
+    public Sprite happy;
+    public Sprite sad;
+    public Sprite confused;
+    public Sprite forlorn;
+    public Sprite terror;
+    
+    public Sprite GetSelectedEmotion(string emotion)
+    {
+        switch (emotion)
+        {
+            case "happy":
+                return happy;
+            case "sad":
+                return sad;
+            case "confused":
+                return confused;
+            case "forlorn":
+                return forlorn;
+            case "terror":
+                return terror;
+            default:
+                return characterDefault;
+        }
+    }
 }
+
+
--- Assets/YarnSpinner/Actors/ShowCharacters.cs
+++ Assets/YarnSpinner/Actors/ShowCharacters.cs
@@ -1,29 +1,33 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using Yarn.Unity;
 
+
 public class ShowCharacters : MonoBehaviour
 {
+
     public Image LeftActorImage;
     public Image RightActorImage;
     
-    public Sprite[] CharacterSprites;
+    public ActorSprites[] actorReferences;
     
     [YarnCommand("actor")]
-    public void ShowActor(int actorId, bool left = true)
+    public void ShowActor(int actorId, string emotion = "none", bool left = true)
     {
         if (left)
         {
             LeftActorImage.enabled = true;
-            LeftActorImage.sprite = CharacterSprites[actorId];
+            LeftActorImage.sprite = actorReferences.First(x => x.actorID == actorId).GetSelectedEmotion(emotion.ToLower());
         }
         else
         {
             RightActorImage.enabled = true;
-            RightActorImage.sprite = CharacterSprites[actorId];
+            RightActorImage.sprite = actorReferences.First(x => x.actorID == actorId).GetSelectedEmotion(emotion.ToLower());
         }
     }
     
+
     [YarnCommand("hideActor")]
     public void HideActor(bool left = true)
     {
