d080cc4c7188f62c5b0dc9445b837e914277ee1a
MalinaZhnita <simon.kondela@gmail.com>
2026-03-14 21:42:43 +0000

    Functional, issue with display
    inputsystem is suprisingly difficult to work with

--- Assets/Code/Scripts/UI/Menu/RebindKeyUI.cs
+++ Assets/Code/Scripts/UI/Menu/RebindKeyUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,20 +8,41 @@ public class RebindKeyUI : MonoBehaviour
     private InputActionRebindingExtensions.RebindingOperation _rebindingOperation;
 
     public InputActionAsset inputAsset;
+    public TextMeshProUGUI buttonTextTMP; 
+    public string waitText;
+
+    void OnEnable()
+    {
+        SetButtonNameToBinding();
+    }
 
     public void RebindKeyStart()
     {
         inputAsset.Disable();
+        buttonTextTMP.text = waitText;
 
-        _rebindingOperation = inputActionRef.ToInputAction().PerformInteractiveRebinding().OnComplete(operation => RebindKeyFinish());
+        _rebindingOperation = inputActionRef.action.PerformInteractiveRebinding().OnComplete(operation => RebindKeyFinish());
 
         _rebindingOperation.Start();
     }
 
-    public void RebindKeyFinish()
+    void RebindKeyFinish()
     {
         _rebindingOperation.Dispose();
 
         inputAsset.Enable();
+
+        SetButtonNameToBinding();
+    }
+
+    void SetButtonNameToBinding()
+    {
+        //int bindingIndex = inputActionRef.action.GetBindingIndexForControl(inputActionRef.action.controls[0]);
+
+        //buttonTextTMP.text = InputControlPath.ToHumanReadableString(inputActionRef.action.bindings[bindingIndex].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+        var bindingIndex = inputActionRef.action.GetBindingIndex(InputBinding.MaskByGroups("Gamepad"));
+    
+        buttonTextTMP.text = inputActionRef.action.GetBindingDisplayString(bindingIndex);
     }
 }
