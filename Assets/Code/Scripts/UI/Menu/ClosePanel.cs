961f27b580b8658ee055d2ed64be2bef43de583f
MalinaZhnita <simon.kondela@gmail.com>
2026-03-15 20:19:16 +0000

    Tweaning UI

--- Assets/Code/Scripts/UI/Menu/ClosePanel.cs
+++ Assets/Code/Scripts/UI/Menu/ClosePanel.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 public class ClosePanel : MonoBehaviour
 {
+    public float closeTime = 0.1f;
     public void CloseThisPanel()
     {
         //expand this by adding animation triggers and etc.
-        gameObject.SetActive(false);
+        gameObject.LeanScale(Vector3.zero , closeTime).setOnComplete(() => gameObject.SetActive(false)).setEaseInOutSine();
+        
+        ;
+    }
+    private void Awake()
+    {
+        gameObject.transform.localScale = Vector3.zero;
+    }
+
+    private void OnEnable()
+    {
+        gameObject.LeanScale(Vector3.one , closeTime);
     }
 }
