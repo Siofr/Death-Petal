e8c3adc3062383bb72fb14e34ba6c2dd8ab01628
None <120342992+Siofr@users.noreply.github.com>
2026-03-17 01:55:25 +0000

    Tutorial UI Animated

--- Assets/Code/Scripts/UI/UITutorial.cs
+++ Assets/Code/Scripts/UI/UITutorial.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
 using System.Linq;
+using DG.Tweening;
 
 public class UITutorial : MonoBehaviour
 {
@@ -14,9 +15,15 @@ public class UITutorial : MonoBehaviour
     private EventBindings<AdvanceTutorialEvent> _advanceTutorialEvent;
     private EventBindings<EndTutorialEvent> _endTutorialEvent;
 
+    // Animation
+    private Sequence _animationSequence;
+    private float _xStartPos;
+    private GameObject _fadeOutObject;
+    private Dictionary<string, string> _savedDict;
+
     private void Awake()
     {
-        _tutorialTriggerListener = new EventBindings<TutorialTriggerEvent>(ShowTutorial);
+        _tutorialTriggerListener = new EventBindings<TutorialTriggerEvent>(OnTutorialTrigger);
         _advanceTutorialEvent = new EventBindings<AdvanceTutorialEvent>(AdvanceStep);
         _endTutorialEvent = new EventBindings<EndTutorialEvent>(EndTutorial);
 
@@ -26,6 +33,8 @@ public class UITutorial : MonoBehaviour
         {
             _tutorialPopups.Add(item.gameObject, item.GetChild(0).GetComponent<TMP_Text>());
         }
+
+        _xStartPos = _tutorialPopups.ElementAt(0).Key.transform.position.x;
     }
 
     private void OnEnable()
@@ -44,19 +53,34 @@ public class UITutorial : MonoBehaviour
 
     private void AdvanceStep(AdvanceTutorialEvent ctx)
     {
-        _tutorialReferences[ctx.actionName].SetActive(false);
+        _animationSequence = DOTween.Sequence();
+        AnimateOut(_tutorialReferences[ctx.actionName]);
+        _fadeOutObject = _tutorialReferences[ctx.actionName];
+        _animationSequence.OnComplete(ResetPosition);
     }
 
-    private void ShowTutorial(TutorialTriggerEvent ctx)
+    private void ShowTutorial()
     {
+        _animationSequence = DOTween.Sequence();
         _tutorialReferences.Clear();
 
-        for(int i = 0; i < ctx.tutorialSteps.Count;  i++)
+        for(int i = 0; i < _savedDict.Count;  i++)
         {
             _tutorialPopups.ElementAt(i).Key.SetActive(true);
-            _tutorialPopups.ElementAt(i).Value.text = ctx.tutorialSteps.ElementAt(i).Key;
-            _tutorialReferences.Add(ctx.tutorialSteps.ElementAt(i).Value, _tutorialPopups.ElementAt(i).Key);
+            AnimateIn(_tutorialPopups.ElementAt(i).Key);
+            _tutorialPopups.ElementAt(i).Value.text = _savedDict.ElementAt(i).Key;
+            _tutorialReferences.Add(_savedDict.ElementAt(i).Value, _tutorialPopups.ElementAt(i).Key);
         }
+
+        _animationSequence.Play();
+    }
+
+    private void OnTutorialTrigger(TutorialTriggerEvent ctx)
+    {
+        _savedDict = ctx.tutorialSteps;
+
+        if (_animationSequence.IsActive()) _animationSequence.OnComplete(ShowTutorial);
+        else ShowTutorial();
     }
 
     private void EndTutorial()
@@ -67,5 +91,35 @@ public class UITutorial : MonoBehaviour
         }
 
         _tutorialReferences.Clear();
+    }
+
+    private void AnimateIn(GameObject go)
+    {
+        go.GetComponent<CanvasGroup>().alpha = 1f;
+        _animationSequence
+            .Append(go.transform.DOMoveX(200f, 0.15f))
+            .Pause();
+    }
+
+    private void AnimateOut(GameObject go)
+    {
+        _animationSequence
+            .Append(
+            go.GetComponent<CanvasGroup>()
+            .DOFade(0, 0.15f))
+            .Play();
+    }
+
+    private void ResetPosition()
+    {
+        _fadeOutObject.transform.position = new Vector3(
+            _xStartPos,
+            _fadeOutObject.transform.position.y,
+            _fadeOutObject.transform.position.z);
+    }
+
+    private void TestMethod()
+    {
+
     }
 }
