cc151998efd650d611ba4ade85c2a1db1c0e57b4
MalinaZhnita <simon.kondela@gmail.com>
2026-04-12 17:17:18 +0100

    health base & folder refactor

---
+++ Assets/Art/UI/Health/HealthUiHearts.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUiHearts : MonoBehaviour
+{
+    private IEntity _healthData;
+    public int maxHp = 3;
+    
+    public GameObject player;
+
+    public GameObject healthImage;
+
+    public List<GameObject> _hearts;
+
+    void OnEnable()
+    {
+        _healthData = player.GetComponent<IEntity>();
+
+        //maxHp = _healthData.Weaknesses.Count;
+
+        //InnitHearts();
+        InnitVisuals();
+    }
+
+    void InnitHearts()
+    {
+        bool first = true;
+        foreach(var child in GetComponentsInChildren<Transform>())
+        {
+            if(!first)
+            {
+                Destroy(child.gameObject);
+            }
+            else
+            {
+                first = false;
+            }
+        } 
+
+        for (int i = 0; i < maxHp; i++)
+        {
+            var currentHeart = Instantiate(healthImage);
+            _hearts.Add(currentHeart);
+
+            currentHeart.transform.parent = transform;
+
+            //print("[HEARTS] : innit heart " + i);
+        }
+    }
+
+    void InnitVisuals()
+    {
+        for (int i = 0; i < _hearts.Count; i++)
+        {
+            Material tempMat = _hearts[i].GetComponent<RawImage>().material;
+
+            tempMat.SetFloat("_Index", 9 + (i % 3));
+
+            _hearts[i].GetComponent<RawImage>().material = tempMat;
+        }
+    }
+
+    void Update()
+    {
+        //TODO: Trigger this on an a player damage event!
+        UpdateHealthPedals();
+    }
+
+    void UpdateHealthPedals()
+    {
+        for (int i = 0; i < maxHp; i++)
+        {
+            print(i);
+            _hearts[i].GetComponent<RawImage>().enabled = i < (_healthData.Weaknesses.Count);
+        }
+
+        //print("[HP] " + _healthData.Weaknesses.Count);
+    }
+}
