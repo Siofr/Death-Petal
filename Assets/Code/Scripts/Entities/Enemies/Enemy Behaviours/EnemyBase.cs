499bf0172fa49c97801eb88919580377d43e31d7
nkoles <37773744+nkoles@users.noreply.github.com>
2026-04-30 17:43:19 +0100

    Pausing Bugs fixed

--- Assets/Code/Scripts/Entities/Enemies/Enemy Behaviours/EnemyBase.cs
+++ Assets/Code/Scripts/Entities/Enemies/Enemy Behaviours/EnemyBase.cs
@@ -195,7 +195,16 @@ public class EnemyBase : EntityBase, IEntity
     private void OnPause(PauseEvent ctx)
     {
         StopAgent(ctx.isPaused);
+        
         animator.speed = ctx.isPaused ? 0 : 1;
+
+        if (!ctx.isPaused)
+        {
+            if (target == null && animator.GetBool(Animator.StringToHash("Spawning")))
+            {
+                animator.speed = 0;
+            }
+        }
     }
 
     protected override void OnCameraChange(CameraChangeEvent ctx)
