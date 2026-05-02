bedc6b67afaaf2c251771e03d55e2ca4b6d856c2
Jess <everydyingplanet@gmail.com>
2026-05-02 22:05:55 +0100

    Bishop Death Animation

--- Assets/Code/Scripts/Entities/Enemies/Boss Behaviours/Boss States/BossDefeatState.cs
+++ Assets/Code/Scripts/Entities/Enemies/Boss Behaviours/Boss States/BossDefeatState.cs
@@ -6,10 +6,8 @@ public class BossDefeatState<T> : BossBaseState<T> where T : BossBase
 	
     public override void OnEnter()
     {
-        Debug.Log("BOSS DIE");
         bossController.StopAllCoroutines();
         bossController.animator.SetFloat(Animator.StringToHash("Blend"),0f);
-        EventBus<OnBossKilledEvent>.Raise(new OnBossKilledEvent());
         bossController.ClearPath();
     }
 }
